86f266856f0e4dfa896d75f15904323320ee0235
Anish karki <karkianish05@gmail.com>
2024-03-27 11:17:36 +0545

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from 'cors'
+import path from 'path'
 import config from "./config/config.js";
 import dbConnection from "./database.js";
 import {app,server} from './socket/socket.js'
@@ -8,6 +9,7 @@ import authRoutes from "./routes/auth.route.js";
 import msgRoutes from "./routes/message.route.js";
 import userRoutes from "./routes/user.route.js";
 
+const __dirname = path.resolve()
 
 app.use(express.json());
 app.use(cookieParser());
@@ -15,7 +17,10 @@ app.use(cors({ origin: true, credentials: true }))
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", msgRoutes);
 app.use("/api/users", userRoutes);
-
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
+app.get('*',(req,res)=>{
+  res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
+})
 server.listen(config.PORT, () => {
   console.log("Server is running on port " + config.PORT);
   dbConnection();
